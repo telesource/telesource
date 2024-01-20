70ee5dbe8ae30127dbc080a10429293d27842557
databoosttechie <databoosttechie@gmail.com>
2024-01-21 00:47:47 +0530

    footer color changed

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,8 @@ import { Container, Typography, Box } from '@mui/material';
 
 export default function Footer() {
   return (
-    <Box sx={{ bgcolor: 'primary.main', color: 'white', p: 3 }} className='footer'>
+    <Box sx={{ bgcolor: 'primary.main', color: 'white', p: 3 }} style={{  backgroundColor: '#6837f5',
+      color: '#FFFFF', padding: '4px'}}>
       <Container maxWidth="lg">
         <Typography variant="body1" align="center">
           © {new Date().getFullYear()} TelesourceNow
