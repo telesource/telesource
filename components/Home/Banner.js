174710f2024f735b16f840c69459cebafb4aeba1
databoosttechie <databoosttechie@gmail.com>
2024-01-23 14:38:42 +0530

    banner changed

--- components/Home/Banner.js
+++ components/Home/Banner.js
@@ -1,18 +1,66 @@
-import React from 'react'
-import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
+import { Box, Typography, Grid } from '@mui/material';
 
 const Banner = () => {
-  return (
-    <div>
-        <Image 
-            src="/images/banner.png" // Route of the image file
-            alt="Description of the image"
-            width={100}  // Desired width
-            height={100} // Desired height
-            layout="responsive" // Makes the image responsive
-        />
-    </div>
-  )
-}
+  const [typingText, setTypingText] = useState('');
+  const fullText = 'The Next Level Of Communication Platform.<br />Empowering enterprises.';
+  const typingSpeed = 100; // Speed of typing in milliseconds
+  const resetDelay = 3000; // Delay before resetting the text
 
-export default Banner
+  useEffect(() => {
+    let index = 0;
+    let isCancelled = false;
+
+    const typeText = () => {
+      if (isCancelled) return;
+
+      if (index < fullText.length) {
+        setTypingText((prev) => prev + fullText.charAt(index));
+        index++;
+        setTimeout(typeText, typingSpeed);
+      } else {
+        setTimeout(() => {
+          if (isCancelled) return;
+          setTypingText('');
+          index = 0;
+          typeText(); // Restart typing
+        }, resetDelay);
+      }
+    };
+
+    typeText();
+
+    // Cleanup function
+    return () => {
+      isCancelled = true;
+    };
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        position: 'relative',
+        height: 500, // Adjust as needed
+        backgroundImage: 'linear-gradient(to right, rgba(0,0,0,0.8), rgba(0,0,0,0.4)), url(/images/banner.png)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <Grid container>
+        <Grid item xs={12} md={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Typography variant="h4" 
+          sx={{ p: 2 }} 
+          dangerouslySetInnerHTML={{ __html: typingText }}  
+          style={{ padding: '10px', color: '#FFF'}}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Banner;
+
