3aadd004c2c217b49db31c085e2fb61e4df00952
databoosttechie <databoosttechie@gmail.com>
2024-01-23 16:47:07 +0530

    contact page modified

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,14 +1,14 @@
 import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
-import { Box, Card, CardContent, TextField, Button, Typography, Grid } from '@mui/material';
-import Layout from '../components/Layout';
-import PageBanner from '@/components/PageBanner';
+import { TextField, Button, Box, Grid } from '@mui/material';
 
+import PageBanner from '@/components/PageBanner';
+import Layout from '../components/Layout';
 import RoomIcon from '@mui/icons-material/Room';
 import PhoneIcon from '@mui/icons-material/Phone';
 import EmailIcon from '@mui/icons-material/Email';
 
-export default function ContactPage() {
+export default function ContactUs() {
   const form = useRef();
   const [message, setMessage] = useState('');
 
@@ -30,112 +30,84 @@ export default function ContactPage() {
       );
   };
 
-
-
   return (
     <Layout>
       <PageBanner />
       <Box
-        sx={{
-          height: '80vh', // Adjust as needed
-          backgroundImage: 'linear-gradient(to right, rgba(0,0,0,0.8), rgba(0,0,0,0.8)), url(/images/banner.png)',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        style={{ backgroundColor: '#eee', padding: '40px 20px' }}
       >
-        <Grid container spacing={2} justifyContent="center" style={{ color: '#FFF'}}>
-          <Grid item xs={12} md={6}>
-            <Typography variant="h3" gutterBottom component="div">
-              Contact Info
-            </Typography>
-            <Box mt={2} style={{ paddingLeft: '20px'}}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} md={6}>
-                <Typography variant="h6" gutterBottom  style={{ color: '#6837f5'}}>
-                <PhoneIcon /> Phone Number
-                </Typography>
-                <Typography variant="body1">
-                  <a href="tel:+91 80808 76443">+91 80808 76443</a>
-                </Typography>
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Typography variant="h6" gutterBottom style={{ color: '#6837f5'}}>
-                <EmailIcon /> Email ID
-                </Typography>
-                <Typography variant="body1">
-                  <a href="mailto:info@telesourcenow.com">info@telesourcenow.com</a>
-                </Typography>
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Typography variant="h6" gutterBottom style={{ color: '#6837f5'}}>
-                <RoomIcon /> Pune Address
-                </Typography>
-                <Typography variant="body1">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <h1 variant="h4" gutterBottom>
+              Contact Us
+            </h1>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <div style={{ flex: 1 }}>
+                <h1 variant="h3" gutterBottom style={{ marginTop: '40px', fontSize: '18px' }}>
+                  <RoomIcon /> Pune Address:-
+                </h1>
+                <p style={{ padding: '2px 5px'}}>
                   Office No: 102BB, Turning Point NX S.No: 206/3, Plot No: 17, behind Phoenix Marketcity, Viman Nagar,
                   Pune - 411014
-                </Typography>
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Typography variant="h6" gutterBottom style={{ color: '#6837f5'}}>
-                <RoomIcon /> Bangalore Address
-                </Typography>
-                <Typography variant="body1">
-                 Office no. 404A Arvind Shambabhin Devrachikenhalli Sennappa Layout Bommanahalli Bangalore - 560068                </Typography>
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Typography variant="h6" gutterBottom style={{ color: '#6837f5'}}>
-                <RoomIcon /> Gurugram Address
-                </Typography>
-                <Typography variant="body1">
+                </p>
+              </div>
+              <div style={{ flex: 1 }}>
+                <h1 variant="h3" gutterBottom style={{ marginTop: '40px', fontSize: '18px' }}>
+                  <RoomIcon /> Bangalore Address: -
+                </h1>
+                <p style={{ padding: '2px 5px'}}>
+                  Office no. 404A Arvind Shambabhin Devrachikenhalli Sennappa Layout Bommanahalli Bangalore - 560068
+                </p>
+              </div>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <div style={{ flex: 1 }}>
+                <h1 variant="h3" gutterBottom style={{ marginTop: '40px', fontSize: '18px' }}>
+                <RoomIcon /> Gurugram Address:-
+                </h1>
+                <p style={{ padding: '2px 5px'}}>
                 Office no.201, New Office Bajghera Road, Ward No.7, Rajendra Park, Palam Vihar, Gurugram, Haryana 122006
-                </Typography>
-              </Grid>
-            </Grid>
-          </Box>
+                </p>
+              </div>
+              <div style={{ flex: 1 }}>
+              <h1 variant="h3" gutterBottom style={{ marginTop: '40px', fontSize: '18px' }}>
+              <PhoneIcon /> Phone Number:-
+            </h1>
+            <p>
+              <a href="tel:+91 80808 76443">+91 80808 76443</a>
+            </p>
+            <h1 variant="h3" gutterBottom style={{ marginTop: '10px', fontSize: '18px' }}>
+              <EmailIcon /> Email ID:-
+            </h1>
+            <p>
+              <a href="mailto:info@telesourcenow.com">info@telesourcenow.com</a>
+            </p>
+              </div>
+            </div>
+
           </Grid>
-          <Grid item xs={12} md={6}>
-            <Card raised sx={{ maxWidth: 450, mx: 'auto' }}>
-              <CardContent>
-                <Typography variant="h4" gutterBottom component="div">
-                  Send Us a Message
-                </Typography>
-                <form ref={form} onSubmit={sendEmail}>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                      <TextField label="First Name" fullWidth margin="normal" name="first_name" />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <TextField label="Last Name" fullWidth margin="normal" name="last_name" />
-                    </Grid>
-                  </Grid>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                      <TextField label="Company Name" fullWidth margin="normal" name="first_name" />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <TextField label="Work Email" fullWidth margin="normal" name="last_name" />
-                    </Grid>
-                  </Grid>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12} sm={6}>
-                      <TextField label="Mobile Number" fullWidth margin="normal" name="first_name" />
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <TextField label="Services & Product" fullWidth margin="normal" name="last_name" />
-                    </Grid>
-                  </Grid>
-                  <TextField
-                    label="Notes"
-                    fullWidth
-                    margin="normal"
-                    multiline
-                    rows={2}
-                    name="Notes"
-                  />
-                  <Box display="flex" justifyContent="center" marginTop={2}>
+          <Grid item xs={12} sm={6}>
+            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" textAlign="center">
+              <h1 style={{ paddingBottom: '15px'}}>Contact Us</h1>
+              <form ref={form} onSubmit={sendEmail}>
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  <TextField label="First Name" name="first_name" fullWidth required size="small" style={{ marginLeft: '3px', marginRight: '3px'}} />
+                  <TextField label="Last Name" name="last_name" fullWidth required size="small" style={{ marginLeft: '3px', marginRight: '3px'}} />
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  <TextField label="Company Name" name="company_name" fullWidth margin="normal" required size="small" style={{ marginLeft: '3px', marginRight: '3px'}} />
+                  <TextField label="Work Email" name="work_email" fullWidth margin="normal" required size="small" style={{ marginLeft: '3px', marginRight: '3px'}} />
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  <TextField label="Mobile Number" name="mobile_number" fullWidth margin="normal" required  size="small" style={{ marginLeft: '3px', marginRight: '3px'}}/>
+                  <TextField label="Services & Product" name="service_and_product" fullWidth margin="normal" size="small" style={{ marginLeft: '3px', marginRight: '3px'}} />
+                </div>
+                <TextField label="Notes" name="notes" fullWidth margin="normal" multiline rows={1} style={{ marginLeft: '3px', marginRight: '3px'}} />
+                <Box display="flex" justifyContent="center" marginTop={2}>
                   <Button
                     type="submit"
                     value="Send"
@@ -143,12 +115,14 @@ export default function ContactPage() {
                     size="large"
                     style={{ backgroundColor: '#6837f5', marginTop: '20px' }}
                   >
-                    Send Message
+                    Submit
                   </Button>
                 </Box>
-                </form>
-              </CardContent>
-            </Card>
+                {message && (
+                  <div style={{ color: message === 'Message Sent Successfully' ? 'green' : 'red' }}>{message}</div>
+                )}
+              </form>
+            </Box>
           </Grid>
         </Grid>
       </Box>
